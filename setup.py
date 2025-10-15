f7ff1b9736018ddc1470b21c3b3df8507fbe6d17
Adithya Maurya K R <135137252+adithyamauryakr@users.noreply.github.com>
2025-10-15 19:50:34 +0530

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "F") as f:
+    description = f.read()
+
 setup(
-    name='pace'
+    name='pace',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.24',
+        'python>=3.10'
+
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown'
 )
